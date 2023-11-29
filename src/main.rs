f19abb4ab55f24d581e599679be0c6c088f5a9eb
Kasper G <kasper.goddeau@gmail.com>
2023-11-29 14:11:18 +0100

    Basic client request function created

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 use reqwest;
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    run().await.unwrap();
+}
+
+async fn run() -> Result<(), Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
+
+    let response = client
+        .get("https://codefever.be/")
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    println!("{}", response);
+
+    Ok(())
 }
