1ebc643344e82003aeafebb0fe3c4d9481049b6d
Kasper G <kasper.goddeau@gmail.com>
2023-12-02 13:13:59 +0100

    Made more styled statuscodes

--- src/fuzz.rs
+++ src/fuzz.rs
@@ -1,5 +1,7 @@
 use std::{error::Error, path::Path};
 
+use colored::{ColoredString, Colorize, CustomColor};
+
 use tokio::{fs, io::AsyncReadExt};
 
 use reqwest::{Client, StatusCode, Url};
@@ -15,13 +17,32 @@ pub async fn load_wordlist(path: &Path) -> Result<Vec<u8>, Box<dyn Error>> {
 
 pub async fn fuzz(url: Url, wordlist: String) -> Result<(), Box<dyn Error>> {
     for path in wordlist.lines() {
-        let response = Client::new()
-            .get(format!("{}/{}", url, path))
-            .send()
-            .await?;
+        let response_result = Client::new().get(format!("{}{}", url, path)).send().await;
 
-        println!("{}    {}", path, response.status());
+        let response = match response_result {
+            Ok(t) => t,
+            Err(e) => panic!("Error connecting. ({})", e),
+        };
+
+        println!("{}        {}", style_error_code(response.status()), path);
     }
 
     Ok(())
+}
+
+fn style_error_code(status: StatusCode) -> ColoredString {
+    println!("{}|{}", "test".red(), "test".bright_red());
+    if status.is_informational() {
+        status.to_string().blue()
+    } else if status.is_success() {
+        status.to_string().bright_green()
+    } else if status.is_redirection() {
+        status.to_string().yellow()
+    } else if status.is_client_error() {
+        status.to_string().red()
+    } else if status.is_server_error() {
+        status.to_string().bright_red()
+    } else {
+        status.to_string().white()
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod fuzz;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let raw_wordlist = fuzz::load_wordlist(&Path::new("fuzz.txt")).await?;
+
     fuzz::fuzz(
         reqwest::Url::from_str("http://127.0.0.1:8888")?,
         String::from_utf8_lossy(raw_wordlist.as_slice()).to_string(),
