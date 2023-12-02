1cc528db38d7fc29291d977f85b0c984448ae2a9
Kasper G <kasper.goddeau@gmail.com>
2023-12-02 14:21:32 +0100

    Reset for test

--- src/fuzz.rs
+++ src/fuzz.rs
@@ -1,27 +1,59 @@
 use std::{error::Error, path::Path};
 
+use colored::{ColoredString, Colorize};
+
 use tokio::{fs, io::AsyncReadExt};
 
 use reqwest::{Client, StatusCode, Url};
 
-pub async fn load_wordlist(path: &Path) -> Result<Vec<u8>, Box<dyn Error>> {
+pub async fn load_wordlist(path: &Path, threads: usize) -> Result<Vec<String>, Box<dyn Error>> {
     let mut file = fs::OpenOptions::new().read(true).open(path).await?;
 
-    let mut buf = vec![];
-    file.read_to_end(&mut buf).await?;
+    let mut buf = String::new();
+    file.read_to_string(&mut buf).await?;
 
-    Ok(buf)
+    let total_lines = buf.lines().count();
+
+    let mut result = vec![];
+
+    for i in 0..threads {
+        result.push(
+            buf[(i * (total_lines / threads))
+                ..((i * (total_lines / threads)) + (total_lines / threads))]
+                .to_owned(),
+        );
+    }
+
+    Ok(result)
 }
 
 pub async fn fuzz(url: Url, wordlist: String) -> Result<(), Box<dyn Error>> {
     for path in wordlist.lines() {
-        let response = Client::new()
-            .get(format!("{}/{}", url, path))
-            .send()
-            .await?;
+        let response_result = Client::new().get(format!("{}{}", url, path)).send().await;
 
-        println!("{}    {}", path, response.status());
+        let response = match response_result {
+            Ok(t) => t,
+            Err(e) => panic!("Error connecting. ({})", e),
+        };
+
+        println!("{}        {}", style_error_code(response.status()), path);
     }
 
     Ok(())
+}
+
+fn style_error_code(status: StatusCode) -> ColoredString {
+    if status.is_informational() {
+        status.to_string().blue()
+    } else if status.is_success() {
+        status.to_string().bright_green()
+    } else if status.is_redirection() {
+        status.to_string().yellow()
+    } else if status.is_client_error() {
+        status.to_string().red()
+    } else if status.is_server_error() {
+        status.to_string().bright_red()
+    } else {
+        status.to_string().white()
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,29 @@ mod fuzz;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let raw_wordlist = fuzz::load_wordlist(&Path::new("fuzz.txt")).await?;
-    fuzz::fuzz(
-        reqwest::Url::from_str("http://127.0.0.1:8888")?,
-        String::from_utf8_lossy(raw_wordlist.as_slice()).to_string(),
-    )
-    .await?;
+    let threads = 16;
+
+    let wordlists = fuzz::load_wordlist(&Path::new("fuzz.txt"), threads).await?;
+
+    let mut handles = vec![];
+
+    for wordlist in wordlists {
+        let handle = tokio::spawn(handle(wordlist));
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.await?;
+    }
 
     Ok(())
+}
+
+async fn handle(wordlist: String) {
+    fuzz::fuzz(
+        reqwest::Url::from_str("http://127.0.0.1:8888").unwrap(),
+        wordlist,
+    )
+    .await
+    .unwrap();
 }
